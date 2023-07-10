7d57e6735075506839badf18640e2eede9d72d6e
Xavier <xavierzy@163.com>
2023-07-10 14:35:17 +0800

    Initial commit

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        Assert.assertEquals(-1, calculator.subtract(2, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        Assert.assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        Assert.assertEquals(2, calculator.divide(6, 3));
+    }
+}
